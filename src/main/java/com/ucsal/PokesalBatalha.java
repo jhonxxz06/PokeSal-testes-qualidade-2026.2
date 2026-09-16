5d208c4763038f848efbbe11b1516c69ae14b67d
jhonxxz06 <jhonxxtiktok@gmail.com>
2026-09-16 18:38:29 -0300

    feat: : finalização do switch-case e definição do construtor

--- src/main/java/com/ucsal/PokesalBatalha.java
+++ src/main/java/com/ucsal/PokesalBatalha.java
@@ -5,10 +5,18 @@ public class PokesalBatalha {
     private TreinadorPokesal desafiante2;
     private int contadorTurnos = 0;
     private AsfaltoUcsal tipoAsfalto;
-// criar um método não um construtor talvez
 
     Scanner ler = new Scanner(System.in);
-    public void BatalhaPokesal(TreinadorPokesal desafiante1, TreinadorPokesal desafiante2) {
+
+    public PokesalBatalha(TreinadorPokesal desafiante1, TreinadorPokesal desafiante2){
+        this.desafiante1 = desafiante1;
+        this.desafiante2 = desafiante2;
+        this.tipoAsfalto = AsfaltoUcsal.escolherAsfalto();
+    }
+
+    public void Batalha() {
+        tipoAsfalto.definirVantagens(desafiante1.getPokesal());
+        tipoAsfalto.definirVantagens(desafiante2.getPokesal());
         int opSelecionada;
         if (desafiante1.getPokesal().getSPD() > desafiante2.getPokesal().getSPD()) {
             while (desafiante1.getPokesal().getHP() > 0 && desafiante2.getPokesal().getHP() > 0) {
@@ -26,9 +34,15 @@ public class PokesalBatalha {
                     opSelecionada = ler.nextInt();
                     switch (opSelecionada) {
                         case 1:
-                            desafiante2.getPokesal().PokesalDanoSofrido(desafiante1.getPokesal().getElementosTipagem().CalculoEfetividade(desafiante2.getPokesal().getElementosTipagem());
+                            desafiante2.getPokesal().PokesalDanoSofrido(desafiante1.getPokesal().getElementosTipagem());
+                            break;
                         case 2:
                             desafiante1.getAcessorios().usarPocao(desafiante1,desafiante1.getAcessorios());
+                            break;
+                        case 3:
+                            System.out.println(desafiante1.getNome() + " fugiu da batalha..." + desafiante2.getNome() + " venceu...");
+                            desafiante1.getPokesal().setHP(0);
+                            break;
                     }
                     contadorTurnos += 1;
                 }
@@ -43,8 +57,15 @@ public class PokesalBatalha {
                     opSelecionada = ler.nextInt();
                     switch (opSelecionada) {
                         case 1:
-
+                            desafiante1.getPokesal().PokesalDanoSofrido(desafiante2.getPokesal().getElementosTipagem());
+                            break;
                         case 2:
+                            desafiante2.getAcessorios().usarPocao(desafiante2,desafiante2.getAcessorios());
+                            break;
+                        case 3:
+                            System.out.println(desafiante2.getNome() + " fugiu da batalha..." + desafiante1.getNome() + " venceu...");
+                            desafiante2.getPokesal().setHP(0);
+                            break;
                     }
                     contadorTurnos += 1;
                 }
@@ -64,7 +85,18 @@ public class PokesalBatalha {
                     System.out.println("1 - Atacar Oponente " +
                             "\n 2 - Usar Poção " + desafiante2.getItensUso() + "/2 " +
                             "\n 3 - Fugir da batalha");
-                    switch (12) {
+                    opSelecionada = ler.nextInt();
+                    switch (opSelecionada) {
+                        case 1:
+                            desafiante1.getPokesal().PokesalDanoSofrido(desafiante2.getPokesal().getElementosTipagem());
+                            break;
+                        case 2:
+                            desafiante2.getAcessorios().usarPocao(desafiante2,desafiante2.getAcessorios());
+                            break;
+                        case 3:
+                            System.out.println(desafiante2.getNome() + " fugiu da batalha..." + desafiante1.getNome() + " venceu...");
+                            desafiante2.getPokesal().setHP(0);
+                            break;
                     }
                     contadorTurnos += 1;
                 }
@@ -73,7 +105,18 @@ public class PokesalBatalha {
                     System.out.println("1 - Atacar Oponente " +
                             "\n 2 - Usar Poção " + desafiante1.getItensUso() + "/2 " +
                             "\n 3 - Fugir da batalha");
-                    switch (12) {
+                    opSelecionada = ler.nextInt();
+                    switch (opSelecionada) {
+                        case 1:
+                            desafiante2.getPokesal().PokesalDanoSofrido(desafiante1.getPokesal().getElementosTipagem());
+                            break;
+                        case 2:
+                            desafiante1.getAcessorios().usarPocao(desafiante1,desafiante1.getAcessorios());
+                            break;
+                        case 3:
+                            System.out.println(desafiante1.getNome() + " fugiu da batalha..." + desafiante2.getNome() + " venceu...");
+                            desafiante1.getPokesal().setHP(0);
+                            break;
                     }
                     contadorTurnos += 1;
                 }
