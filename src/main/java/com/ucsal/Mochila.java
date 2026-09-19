583f919c1e0338cc23a4a648554daf0bdd2c73d2
jhonxxz06 <jhonxxtiktok@gmail.com>
2026-09-19 09:38:40 -0300

    fix: verificador de quantidade disponível dos itens de cura antes da aplicação do efeito

--- src/main/java/com/ucsal/Mochila.java
+++ src/main/java/com/ucsal/Mochila.java
@@ -37,21 +37,28 @@ public class Mochila {
 
     public void usarPocao(TreinadorPokesal treinador, Mochila pocao) {
         // ver um verificador de poção
-        System.out.println("Poção Selecionada...");
-        treinador.getPokesal().setHpBatalha((treinador.getPokesal().getHpBatalha() + 5));
-        System.out.println(treinador.getPokesal().getNome() + " teve 5 pnts de vida restaurados");
-        treinador.setItensUso(treinador.getItensUso() + 1);
-        pocao.setQuantidades(getQuantidades() - 1);
-        // pensar para diminuição da quantidade dos itens
+        if (pocao.getQuantidades() > 0) {
+            System.out.println("Poção Selecionada...");
+            treinador.getPokesal().setHpBatalha((treinador.getPokesal().getHpBatalha() + 5));
+            System.out.println(treinador.getPokesal().getNome() + " teve 5 pnts de vida restaurados");
+            treinador.setItensUso(treinador.getItensUso() + 1);
+            pocao.setQuantidades(getQuantidades() - 1);
+            // pensar para diminuição da quantidade dos itens
+        } else {
+            System.out.println(getItem() + " Indisponível");
+        }
     }
 
     public void usarSuperPocao(TreinadorPokesal treinador, Mochila superPocao) {
-        System.out.println("Super Poção Selecionada...");
-        treinador.getPokesal().setHpBatalha((treinador.getPokesal().getHpBatalha() + 10));
-        System.out.println(treinador.getPokesal().getNome() + " teve 10 pnts de vida restaurados");
-        treinador.setItensUso(treinador.getItensUso() + 1);
-        superPocao.setQuantidades(getQuantidades() - 1);
-        // pensar para diminuição da quantidade dos itens
+        if (superPocao.getQuantidades() > 0) {
+            System.out.println("Super Poção Selecionada...");
+            treinador.getPokesal().setHpBatalha((treinador.getPokesal().getHpBatalha() + 10));
+            System.out.println(treinador.getPokesal().getNome() + " teve 10 pnts de vida restaurados");
+            treinador.setItensUso(treinador.getItensUso() + 1);
+            superPocao.setQuantidades(getQuantidades() - 1);
+            // pensar para diminuição da quantidade dos itens
+        } else {
+            System.out.println(getItem() + " Indisponível");
+        }
     }
-
 }
